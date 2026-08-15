fde8c570782215327b2facfc4415d8335ae0c18d
wikimodels <55264797+wikimodels@users.noreply.github.com>
2026-08-15 11:32:31 +0300

    Change default output folder to Downloads\\TrimmedAudio

--- audiotrimmer/config.py
+++ audiotrimmer/config.py
@@ -7,7 +7,7 @@ CONFIG_PATH = Path(__file__).resolve().parent.parent / "config.json"
 
 DEFAULTS = {
     "source": r"C:\Users\Vitali\Downloads\YandexMusic",
-    "output": r"C:\Users\Vitali\Downloads\AudioTrimmer Output",
+    "output": r"C:\Users\Vitali\Downloads\TrimmedAudio",
 }
 
 
